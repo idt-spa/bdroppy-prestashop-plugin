7ade6f860daf68236e0610ab1ea79c469936f418
hamidisaac <isaac.hamid@gmail.com>
2022-06-23 16:52:29 +0430

    bugfix loading css file

--- bdroppy.php
+++ bdroppy.php
@@ -643,8 +643,7 @@ class Bdroppy extends Module
         //return $output . $this->displayForm() . $this->displayPriceForm();
         $catalogs = $this->getCatalogs();
 
-        $stripeBOCssUrl = str_replace('http://', 'https://', _PS_BASE_URL_ . __PS_BASE_URI__) .
-            'modules/bdroppy/views/css/bdroppy.css';
+        $stripeBOCssUrl = _PS_BASE_URL_ . __PS_BASE_URI__. 'modules/bdroppy/views/css/bdroppy.css';
         $base_url = "Unkown";
         $api_key = "Unkown";
         $api_token = "Unkown";
